d63b94945b4728b0b08e5cce567f359e56e041e7
semaanzahe <semaanzahe@gmail.com>
2026-01-30 20:37:25 +0200

    Pause Menu

--- Assets/Hud.cs
+++ Assets/Hud.cs
@@ -1,20 +1,54 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Hud : MonoBehaviour
 {
-     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    
-    }
+
+    public static bool IsPaused = false;
+    public GameObject pauseMenuUI;
 
     // Update is called once per frame
     void Update()
     {
-        
-            
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused)
+            {
+                Resume();
+            }else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        IsPaused = true;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1f;
+    }
+
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;    
+        #else
+         Application.Quit();
+        #endif
     }
 }
